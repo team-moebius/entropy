8b12b369aa7b1c7e11a94d1879e583f86b057fe9
Dongha Kim <ghost9087@gmail.com>
2020-12-16 22:36:48 +0900

    [ETP-0] Initialize project with Spring

---
+++ src/main/java/com/moebius/entropy/EntropyApplication.java
@@ -0,0 +1,12 @@
+package com.moebius.entropy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntropyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EntropyApplication.class, args);
+	}
+
+}
