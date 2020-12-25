4eea7e913308c57080863591734ef18ac7bef8e4
knunu <burette@hanyang.ac.kr>
2020-12-25 18:22:15 +0900

    [ETP-8] Update sample request.

--- src/main/java/com/moebius/entropy/controller/BobooController.java
+++ src/main/java/com/moebius/entropy/controller/BobooController.java
@@ -13,6 +13,18 @@ import reactor.core.publisher.Flux;
 public class BobooController {
 	private final BobooService bobooService;
 
+	/**
+	 * Sample request for testing
+	 *
+	 * GET /boboo/open-orders?symbol=GTAXUSDT HTTP/1.1
+	 * Host: 127.0.0.1:8080
+	 * Content-Type: application/json
+	 *
+	 * {
+	 *     "accessKey": "ulty827N2vakJap9OdAAejTe7VwykXxUplXSbPGhkLq5r5ImeXACdcr3pZhl6inF",
+	 *     "secretKey": "QCzWtRLiuB1vyd1wrmcpZ7m2HywSzasGhQv0olXvUXxpezNhn9RQwFnW2nWYX6lH"
+	 * }
+	 */
 	@GetMapping("/open-orders")
 	public Flux<BobooOpenOrdersDto> getOpenOrders(@RequestParam String symbol, @RequestBody ApiKeyDto apiKeyDto) {
 		return bobooService.getOpenOrders(symbol, apiKeyDto);
