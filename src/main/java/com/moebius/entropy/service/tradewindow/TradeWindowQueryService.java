6b307ee0e4e7bc414b74e22ab2706958c9668718
Dongha Kim <caffeine.driven@outlook.com>
2020-12-25 23:53:46 +0900

    [ETP-2] Add TradeWindowQueryService and TradeWindowRepository

--- src/main/java/com/moebius/entropy/service/tradewindow/TradeWindowQueryService.java
+++ src/main/java/com/moebius/entropy/service/tradewindow/TradeWindowQueryService.java
@@ -2,16 +2,23 @@ package com.moebius.entropy.service.tradewindow;
 
 import com.moebius.entropy.domain.Market;
 import com.moebius.entropy.domain.TradeWindow;
+import com.moebius.entropy.service.tradewindow.repository.TradeWindowRepository;
 import java.math.BigDecimal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+@Component
+@RequiredArgsConstructor
 public class TradeWindowQueryService {
 
+    private final TradeWindowRepository repository;
+
     public Mono<TradeWindow> fetchTradeWindow(Market market) {
-        return null;
+        return Mono.justOrEmpty(repository.getTradeWindowForSymbol(market));
     }
 
     public BigDecimal getMarketPrice(Market market) {
-        return null;
+        return repository.getMarketPriceForSymbol(market);
     }
 }
