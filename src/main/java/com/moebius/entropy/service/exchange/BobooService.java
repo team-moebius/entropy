134c079ed5009f90ffcf463c10afcea5c84b4b31
knunu <burette@hanyang.ac.kr>
2021-01-01 23:52:34 +0900

    [ETP-3] Initialize ETP-3

--- src/main/java/com/moebius/entropy/service/exchange/BobooService.java
+++ src/main/java/com/moebius/entropy/service/exchange/BobooService.java
@@ -6,7 +6,6 @@ import com.moebius.entropy.dto.exchange.order.boboo.BobooOpenOrdersDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.socket.client.WebSocketClient;
