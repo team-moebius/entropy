150b5123ddf618a966ff0b63dfc2f9264b34a941
knunu <burette@hanyang.ac.kr>
2021-04-02 17:47:41 +0900

    [ETP-29] Add inflation logic to optimize order service.

--- src/main/java/com/moebius/entropy/service/order/boboo/auto/BobooOptimizeOrderService.java
+++ src/main/java/com/moebius/entropy/service/order/boboo/auto/BobooOptimizeOrderService.java
@@ -3,13 +3,13 @@ package com.moebius.entropy.service.order.boboo.auto;
 import com.moebius.entropy.domain.Market;
 import com.moebius.entropy.domain.order.Order;
 import com.moebius.entropy.domain.order.OrderRequest;
+import com.moebius.entropy.service.exchange.boboo.BobooExchangeService;
 import com.moebius.entropy.service.order.boboo.BobooOrderService;
 import com.moebius.entropy.service.tradewindow.TradeWindowInflationVolumeResolver;
 import com.moebius.entropy.service.tradewindow.TradeWindowQueryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Flux;
 
 import java.math.BigDecimal;
@@ -20,13 +20,16 @@ import java.time.Duration;
 @RequiredArgsConstructor
 public class BobooOptimizeOrderService {
 	private final static long DEFAULT_DELAY = 300L;
-	private final TradeWindowQueryService tradeWindowQueryService;
 	private final BobooOrderService orderService;
+	private final BobooExchangeService exchangeService;
+	private final TradeWindowQueryService tradeWindowQueryService;
 	private final TradeWindowInflationVolumeResolver volumeResolver;
 
 	public Flux<Order> optimizeOrders(Market market) {
 		BigDecimal marketPrice = tradeWindowQueryService.getMarketPrice(market);
 		BigDecimal highestBidPrice = marketPrice.subtract(market.getTradeCurrency().getPriceUnit());
+
+		exchangeService.inflateOrdersByOrderBook(market.getSymbol());
 
 		return orderService.fetchOpenOrdersFor(market)
 			.delayElements(Duration.ofMillis(DEFAULT_DELAY))
