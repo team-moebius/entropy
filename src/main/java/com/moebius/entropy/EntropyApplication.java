64e279ec09f29f6ff9c93831a998c27c99d40f54
knunu <burette@hanyang.ac.kr>
2021-04-02 18:00:55 +0900

    [ETP-29] Remove inflation process when app started.

--- src/main/java/com/moebius/entropy/EntropyApplication.java
+++ src/main/java/com/moebius/entropy/EntropyApplication.java
@@ -1,26 +1,13 @@
 package com.moebius.entropy;
 
-import com.moebius.entropy.service.exchange.ExchangeService;
-import java.util.List;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
 
 @Slf4j
 @SpringBootApplication
-@RequiredArgsConstructor
-public class EntropyApplication implements ApplicationListener<ApplicationReadyEvent> {
-	private final List<ExchangeService> exchangeServices;
-
+public class EntropyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EntropyApplication.class, args);
-	}
-
-	@Override
-	public void onApplicationEvent(ApplicationReadyEvent event) {
-		exchangeServices.forEach(exchangeService -> exchangeService.inflateOrdersByOrderBook("ETHVUSDT"));
 	}
 }
