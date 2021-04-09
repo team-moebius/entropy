1a000003fb1bf22f67dfe304e765c2d4fcccfc5d
Seonwoo Kim <knunu92@gmail.com>
2021-04-09 22:34:52 +0900

    [ETP-36] (#38)
    * [ETP-36] Ignore specific error from exchange while cancel orders
    
    * [ETP-36] Remove order tracking when request manual orders
    
    * [ETP-36] Remove all kind of order tracking
    
    * [ETP-36] Fetch openOrders to cancel inflation orders
    
    Co-authored-by: ghost9087 <ghost9087@gmail.com>

--- src/test/groovy/com/moebius/entropy/service/order/OrderServiceTestSpec.groovy
+++ src/test/groovy/com/moebius/entropy/service/order/OrderServiceTestSpec.groovy
@@ -19,7 +19,6 @@ import reactor.test.StepVerifier
 import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Subject
-import spock.lang.Unroll
 
 @SuppressWarnings(['GroovyAssignabilityCheck', 'GroovyAccessibility'])
 class OrderServiceTestSpec extends Specification {
@@ -59,17 +58,6 @@ class OrderServiceTestSpec extends Specification {
                 .assertNext({ assertOrderWithRequest(it, orderRequest) })
                 .verifyComplete()
 
-        StepVerifier.create(sut.fetchAllOrdersFor(market))
-                .assertNext({ assertOrderWithRequest(it, orderRequest) })
-                .verifyComplete()
-
-        StepVerifier.create(sut.fetchAutomaticOrdersFor(market))
-                .assertNext({ assertOrderWithRequest(it, orderRequest) })
-                .verifyComplete()
-
-        StepVerifier.create(sut.fetchManualOrdersFor(market))
-                .verifyComplete()
-
         where:
         orderPosition << [OrderPosition.ASK, OrderPosition.BID]
     }
@@ -90,16 +78,6 @@ class OrderServiceTestSpec extends Specification {
                 .assertNext({ assertOrderWithRequest(it, orderRequest) })
                 .verifyComplete()
 
-        StepVerifier.create(sut.fetchAllOrdersFor(market))
-                .assertNext({ assertOrderWithRequest(it, orderRequest) })
-                .verifyComplete()
-
-        StepVerifier.create(sut.fetchManualOrdersFor(market))
-                .assertNext({ assertOrderWithRequest(it, orderRequest) })
-                .verifyComplete()
-
-        StepVerifier.create(sut.fetchAutomaticOrdersFor(market))
-                .verifyComplete()
 
         where:
         orderPosition << [OrderPosition.ASK, OrderPosition.BID]
@@ -107,8 +85,6 @@ class OrderServiceTestSpec extends Specification {
 
     def "Cancel order traced by OrderService"() {
         given:
-        def orderTrackedByService = new Order(orderId, market, orderPosition, price, volume)
-        addOrderList([orderTrackedByService])
         def orderShouldBeCancelled = new Order(orderId, market, orderPosition, price, volume)
 
         1 * mockExchangeService.cancelOrder(_, {
@@ -127,123 +103,6 @@ class OrderServiceTestSpec extends Specification {
         orderPosition << [OrderPosition.ASK, OrderPosition.BID]
     }
 
-    def "Cancel order not traced by OrderService"() {
-        def orderShouldBeCancelled = new Order(orderId, market, orderPosition, price, volume)
-
-        0 * mockExchangeService.cancelOrder(_, _)
-        0 * mockAssembler.convertToCancelRequest(orderShouldBeCancelled)
-
-        expect:
-        StepVerifier.create(sut.cancelOrderWithoutTracking(orderShouldBeCancelled))
-                .verifyComplete()
-
-        where:
-        orderPosition << [OrderPosition.ASK, OrderPosition.BID]
-    }
-
-    @Unroll
-    def "Update orders from exchange with condition of #comment"() {
-        given:
-        addOrderList(ordersTrackedByService)
-        setAutomaticOrders(automaticOrderIds)
-
-        expect:
-        StepVerifier.create(sut.updateOrders(ordersReceivedFromExchange))
-                .thenConsumeWhile({ true })
-                .verifyComplete()
-
-        StepVerifier.create(sut.fetchAllOrdersFor(market))
-            .recordWith({[]})
-            .thenConsumeWhile({true})
-            .consumeRecordedWith({
-                assert it.size() == ordersReceivedFromExchange.size()
-                return it.stream().allMatch({updatedOrder ->
-                    def orderFromExchange = ordersReceivedFromExchange.find {order-> updatedOrder.orderId == order.orderId}
-                    assert orderFromExchange.price == updatedOrder.price
-                    assert orderFromExchange.volume == updatedOrder.volume
-                    true
-                })
-            })
-            .verifyComplete()
-
-        StepVerifier.create(sut.fetchAutomaticOrdersFor(market))
-                .recordWith({[]})
-                .thenConsumeWhile({true})
-                .consumeRecordedWith({
-                    assert it.stream().allMatch({order->
-                        automaticOrderIdsAfterUpdate.contains(order.orderId)
-                    })
-                })
-                .verifyComplete()
-
-        where:
-        ordersTrackedByService << [
-                [createOrderWith("1", OrderPosition.ASK, 11.11, 111.111),
-                 createOrderWith("2", OrderPosition.BID, 22.22, 222.222),
-                 createOrderWith("3", OrderPosition.ASK, 33.33, 333.333),
-                 createOrderWith("4", OrderPosition.BID, 44.44, 444.444),
-                 createOrderWith("5", OrderPosition.ASK, 55.55, 555.555),
-                ],
-                [createOrderWith("1", OrderPosition.ASK, 11.11, 111.111),
-                 createOrderWith("2", OrderPosition.BID, 22.22, 222.222),
-                ],
-                [createOrderWith("1", OrderPosition.ASK, 11.11, 111.111),
-                 createOrderWith("2", OrderPosition.BID, 22.22, 222.222),
-                 createOrderWith("3", OrderPosition.ASK, 33.33, 333.333),
-                 createOrderWith("4", OrderPosition.BID, 44.44, 444.444),
-                 createOrderWith("5", OrderPosition.ASK, 55.55, 555.555),
-                ],
-                [],
-                []
-        ]
-        automaticOrderIds << [
-                ["1", "2", "3", "4", "5"],
-                ["1",],
-                ["1", "4",],
-                [],
-                [],
-        ]
-        ordersReceivedFromExchange << [
-                [createOrderWith("1", OrderPosition.ASK, 11.11, 111.111),
-                 createOrderWith("2", OrderPosition.BID, 22.22, 222.222),
-                 createOrderWith("3", OrderPosition.ASK, 33.33, 333.333),
-                 createOrderWith("4", OrderPosition.BID, 44.44, 444.444),
-                 createOrderWith("5", OrderPosition.ASK, 55.55, 555.555),
-                ],
-                [createOrderWith("1", OrderPosition.ASK, 11.11, 111.111),
-                 createOrderWith("2", OrderPosition.BID, 22.22, 222.222),
-                 createOrderWith("3", OrderPosition.ASK, 33.33, 333.333),
-                 createOrderWith("4", OrderPosition.BID, 44.44, 444.444),
-                 createOrderWith("5", OrderPosition.ASK, 55.55, 555.555),
-                ],
-                [createOrderWith("1", OrderPosition.ASK, 11.11, 111.111),
-                 createOrderWith("5", OrderPosition.ASK, 55.55, 666.666),
-                ],
-                [],
-                [createOrderWith("1", OrderPosition.ASK, 11.11, 111.111),
-                 createOrderWith("2", OrderPosition.BID, 22.22, 222.222),
-                 createOrderWith("3", OrderPosition.ASK, 33.33, 333.333),
-                 createOrderWith("4", OrderPosition.BID, 44.44, 444.444),
-                 createOrderWith("5", OrderPosition.ASK, 55.55, 555.555),
-                ],
-        ]
-        automaticOrderIdsAfterUpdate << [
-                new HashSet<>(["1", "2", "3", "4", "5"]),
-                new HashSet<>(["1",]),
-                new HashSet<>(["1",]),
-                Collections.<String>emptySet(),
-                Collections.<String>emptySet(),
-        ]
-        comment << [
-                "Nothing is changed",
-                "Someone made order directly from exchange service",
-                "Some of orders are cancelled manually or transaction completed or partial completed",
-                "There's no order on both sides",
-                "Get orders from exchange when no orders are tracked"
-        ]
-
-    }
-
     boolean assertOrderWithRequest(Order order, OrderRequest orderRequest) {
         assert order.orderId != null
         assert order.market == orderRequest.market
@@ -253,18 +112,4 @@ class OrderServiceTestSpec extends Specification {
         return true
     }
 
-    def addOrderList(List<Order> orders) {
-        sut.orderListForSymbol.clear()
-        sut.orderListForSymbol.computeIfAbsent(symbol, { [] })
-            .addAll(orders)
-    }
-
-    Order createOrderWith(String orderId, OrderPosition orderPosition, float price, float volume) {
-        return new Order(orderId, market, orderPosition, BigDecimal.valueOf(price), BigDecimal.valueOf(volume))
-    }
-
-    def setAutomaticOrders(List<String> orderIds) {
-        sut.automaticOrderIds.clear()
-        sut.automaticOrderIds.addAll(orderIds)
-    }
 }
