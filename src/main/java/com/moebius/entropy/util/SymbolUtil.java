47cc4870d6ec76e8437bfa65668bc8ec15f8513e
Seonwoo Kim <knunu92@gmail.com>
2021-05-04 00:36:39 +0900

    [ETP-43] Support DGC on boboo and refactoring to be easy to extend. (#44)

--- src/main/java/com/moebius/entropy/util/SymbolUtil.java
+++ src/main/java/com/moebius/entropy/util/SymbolUtil.java
@@ -1,40 +1,23 @@
 package com.moebius.entropy.util;
 
-import com.moebius.entropy.domain.Exchange;
 import com.moebius.entropy.domain.Market;
-import com.moebius.entropy.domain.trade.TradeCurrency;
+import com.moebius.entropy.domain.Symbol;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Map;
 import java.util.Optional;
-import org.apache.commons.lang3.StringUtils;
 
 public class SymbolUtil {
-    private final static Map<String, Integer> PRICE_DECIMAL_POSITIONS = Map.of(
-        "GTAX2USDT", 2,
-        "ETHVUSDT", 2,
-        "MOIUSDT", 2);
-    private final static Map<String, Integer> VOLUME_DECIMAL_POSITIONS = Map.of(
-        "GTAX2USDT", 2,
-        "ETHVUSDT", 1,
-        "MOIUSDT", 2);
-    public static Market marketFromSymbol(String symbol) {
-        return Optional.ofNullable(symbol)
-            .filter(StringUtils::isNotEmpty)
-            .map(orgSymbol -> {
-                TradeCurrency currency = orgSymbol.endsWith("USDT")
-                    ? TradeCurrency.USDT : TradeCurrency.KRW;
-                return new Market(Exchange.BOBOO, orgSymbol, currency, PRICE_DECIMAL_POSITIONS.getOrDefault(orgSymbol, 0), VOLUME_DECIMAL_POSITIONS.getOrDefault(orgSymbol, 0));
-            })
-            .orElse(null);
-    }
+	private final static Map<String, Market> MARKETS = Symbol.getNameToMarkets();
 
-    public static String stripCurrencyFromSymbol(Market market) {
-        String symbol = market.getSymbol();
-        String currency = market.getTradeCurrency().name();
-        if (symbol.endsWith(currency)) {
-            return symbol.replace(currency, "");
-        } else {
-            return symbol;
-        }
-    }
+	public static Market marketFromSymbol(String symbol) {
+		return Optional.ofNullable(symbol)
+			.filter(StringUtils::isNotEmpty)
+			.map(MARKETS::get)
+			.orElse(null);
+	}
+
+	public static String stripCurrencyFromSymbol(Market market) {
+		return Symbol.valueOf(market.getSymbol()).getKey().toUpperCase();
+	}
 }
