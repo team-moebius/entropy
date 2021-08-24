421ee7f463310022122ae6f8d718f8d169efb710
knunu <burette@hanyang.ac.kr>
2021-08-24 18:22:26 +0900

    [ETP-47] Implement trade window update logic as only updated window comes in at bigone.

--- src/main/java/com/moebius/entropy/service/tradewindow/TradeWindowCommandService.java
+++ src/main/java/com/moebius/entropy/service/tradewindow/TradeWindowCommandService.java
@@ -4,47 +4,87 @@ import com.moebius.entropy.domain.Market;
 import com.moebius.entropy.domain.trade.TradePrice;
 import com.moebius.entropy.domain.trade.TradeWindow;
 import com.moebius.entropy.repository.TradeWindowRepository;
-import java.math.BigDecimal;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class TradeWindowCommandService {
 
-    private final TradeWindowRepository tradeWindowRepository;
+	private final TradeWindowRepository tradeWindowRepository;
 
-    public void saveCurrentTradeWindow(Market market, BigDecimal marketPrice,
-        TradeWindow tradeWindow) {
-        tradeWindowRepository.savePriceForSymbol(market, marketPrice);
+	public void saveCurrentTradeWindow(Market market, BigDecimal marketPrice,
+		TradeWindow tradeWindow) {
+		tradeWindowRepository.savePriceForSymbol(market, marketPrice);
 
-        if (tradeWindow == null ||
-            (CollectionUtils.isEmpty(tradeWindow.getAskPrices()) && CollectionUtils.isEmpty(tradeWindow.getBidPrices()))) {
-            log.warn("[{}-{}] There is an empty trade window, please check the inflation.", market.getExchange(), market.getSymbol());
-            return;
-        }
+		if (tradeWindow == null ||
+			(CollectionUtils.isEmpty(tradeWindow.getAskPrices()) && CollectionUtils.isEmpty(tradeWindow.getBidPrices()))) {
+			log.warn("[{}-{}] There is an empty trade window, please check the inflation.", market.getExchange(), market.getSymbol());
+			return;
+		}
 
-        tradeWindowRepository.saveTradeWindowForSymbol(market, tradeWindow);
-    }
+		TradeWindow toBeSavedWindow = tradeWindow;
+		TradeWindow oldWindow = tradeWindowRepository.getTradeWindowForSymbol(market);
 
-    /** TODO
-     * implement trade window update logic not to replace all, to update the specific trade price having unit price matched with the new trade window's ask or bid valid trade's one.
-     * There are 3 cases here.
-     *
-     * 1. existent trade's BID|ASK volume > new trade's ASK|BID volume : subtract and keep the structure.
-     * 2. existent trade's BID|ASK volume = new trade's ASK|BID volume : remove that matched trade.
-     * 3. existent trade's BID|ASK volume < new trade's ASK|BID volume : remove that matched trade and add the left volume's unit trade to the ASK|BID trades.
-     *
-     * In Bigone, there could be n ASK / n BID tradeWindow (0 volume trade price should be handled with being removed in each ASK or BID windows)
-     * maybe it needs to change the List<TradePrice> to Map<String, TradePrice> for supporting integrated update logic.
-     */
+		if (needUpdateTradeWindow(oldWindow, tradeWindow)) {
+			toBeSavedWindow = updateTradeWindow(oldWindow, tradeWindow);
+		}
 
+		tradeWindowRepository.saveTradeWindowForSymbol(market, toBeSavedWindow);
+	}
+
+	private boolean needUpdateTradeWindow(TradeWindow oldWindow, TradeWindow newWindow) {
+		int oldAskSize = Optional.ofNullable(oldWindow)
+			.map(TradeWindow::getAskPrices)
+			.map(List::size)
+			.orElse(0);
+		int oldBidSize = Optional.ofNullable(oldWindow)
+			.map(TradeWindow::getBidPrices)
+			.map(List::size)
+			.orElse(0);
+
+		return oldAskSize / 2 > newWindow.getAskPrices().size()
+			&& oldBidSize / 2 > newWindow.getBidPrices().size();
+	}
+
+	private TradeWindow updateTradeWindow(TradeWindow oldWindow, TradeWindow updatedWindow) {
+		List<TradePrice> updatedAskPrices = updatedWindow.getAskPrices();
+		List<TradePrice> updatedBidPrices = updatedWindow.getBidPrices();
+
+		List<TradePrice> oldAskPrices = oldWindow.getAskPrices();
+		List<TradePrice> oldBidPrices = oldWindow.getBidPrices();
+
+		oldAskPrices.removeIf(tradePrice -> updatedAskPrices.stream()
+			.anyMatch(getSamePriceAndZeroVolumePredicate(tradePrice)));
+
+		oldBidPrices.removeIf(tradePrice -> updatedBidPrices.stream()
+			.anyMatch(getSamePriceAndZeroVolumePredicate(tradePrice)));
+
+		updatedAskPrices.stream()
+			.filter(getValidVolumePredicate())
+			.forEach(oldAskPrices::add);
+
+		updatedBidPrices.stream()
+			.filter(getValidVolumePredicate())
+			.forEach(oldBidPrices::add);
+
+		return oldWindow;
+	}
+
+	private Predicate<TradePrice> getSamePriceAndZeroVolumePredicate(TradePrice tradePrice) {
+		return newTradePrice -> newTradePrice.getUnitPrice().compareTo(tradePrice.getUnitPrice()) == 0
+			&& newTradePrice.getVolume().compareTo(BigDecimal.ZERO) == 0;
+	}
+
+	private Predicate<TradePrice> getValidVolumePredicate() {
+		return tradePrice -> tradePrice.getVolume().compareTo(BigDecimal.ZERO) > 0;
+	}
 }
